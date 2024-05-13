ef16b8d0c9830cd632f3ba1a9730cbc65e0dd1c2
armandres <armandres@gmail.com>
2024-05-13 10:39:00 -0600

    Las modifications

--- Plots_and_Code_papers/ultimo intento.R
+++ Plots_and_Code_papers/ultimo intento.R
@@ -1,31 +1,35 @@
 getwd()
 setwd("/Users/andresammx/OneDrive/Itzel")
+
+#Required libraries
 library(reshape)
 library(dplyr)
 library(ggplot2)
-#processing of the dataset
-#Creación del objeto base
+
+#Getting the dataset
 l1<-read.csv("lote1.csv", header=TRUE, sep=",", stringsAsFactors=TRUE)
 names(l1)
-#Cambiar al formato largo
+
+#Changing from wide to long format
 fus<-melt(l1, id.vars=c("tx", "id", "sennosen", "sennosen2"))
 levels(fus$variable)<-c("0", "1", "2", "3", "4")
-#Crear un objeto que solamente contenga los datos de tolueno
+
+#Filtering the dataset to get the toluene data
 fus_tol<-subset(fus, tx == "Tol", select=c(tx, id, sennosen, variable, value))
 fus_tol <- droplevels(fus_tol)
-levels(fus_tol$tx)
-#Crear un objeto que solaemnte contenga los datos de aire
+
+#Filtering the dataset to get the air data
 fus_air<-subset(fus, tx == "Air", select=c(tx, id, sennosen, variable,value))
 fus_air<- droplevels(fus_air)
-levels(fus_air$tx)
 
+#Create an object with the names of the categories in the toluene data
 Groups<-c("Toluene non-sensitized", "Toluene sensitized")
 
-#Crear un grafico que contenga boxplot por semana, puntos por semana y lineas por semana. Data are in long format, to perform the change from  wide to long format was used the function XXX from the library reshape
+#Create the plot with toluene data
 p<-ggplot(data = fus_tol, aes(x = variable, y = value)) +
-geom_boxplot(data = fus_tol, aes(x=variable, y=value)) +
-geom_point(aes(color = sennosen, alpha=0.5, group=id)) +
-geom_line(aes(color = sennosen, alpha=0.5, group=id)) +
+geom_boxplot(data = fus_tol, aes(x = variable, y = value)) +
+geom_point(aes(color = sennosen, alpha=0.5, group = id)) +
+geom_line(aes(color = sennosen, alpha=0.5, group = id)) +
 coord_cartesian(ylim = c(0, 500)) + 
 scale_colour_manual(values = c("red", "blue"),  labels = Groups) + 
 labs(x = "Weeks", y = "Locomotor activity (# of crossings/10 min)") + 
@@ -38,73 +42,23 @@ theme(
 	panel.background = element_rect(fill = "white", color = "black"),
 	panel.grid.major = element_blank(),	
 	#panel.grid.minor = element_blank(),
-	legend.title = element_blank())
-p
+	#legend.title = element_blank())
+print(p)
 
-
-_______________________________________
-#Estrategia para superponer dos graficos
-#Primero debo hacer el subset de los datos
-#El objeto "fus" en la variable tx tiene el nivel Air
-_______________________________________
-#unicamente lineas azules
-q<-ggplot(data = fus_air, aes(x = variable, y = value)) + 
-geom_point(color = "blue") + 
-geom_line(aes(group = id), color = "blue") +
-coord_cartesian(ylim = c(0,500)) + 
-labs(x = "Week", y = "Locomotor activity (# crossing / 10 min)") + 
-theme_bw() + 
-theme(legend.title = element_blank())
-q
-
-combined_plot <- facet_grid(p, q, ncol = 1)
-combined_plot
-
-library(ggplot2)
-
-# Gráfico p (Boxplot con puntos y líneas)
-p <- ggplot(data = fus_tol, aes(x = variable, y = value)) +
-  geom_boxplot(aes(color = sennosen, group = id)) +
-  geom_point(data = fus_tol, aes(x = variable, y = value, color = sennosen)) +
-  geom_line(data = fus_tol, aes(x = variable, y = value, color = sennosen)) +
-  labs(x = "Variable", y = "Value") +  # Etiquetas de ejes
-  theme_bw()  # Tema de fondo blanco
-
-# Gráfico q (Puntos y líneas en azul)
-s <- ggplot(data = fus_air, aes(x = variable, y = value)) +
-  geom_point(color = "blue") +
-  #geom_point(aes(alpha = 0.7), color = "blue") +
-  geom_line(aes(group = id), color = "blue") +
-  coord_cartesian(ylim = c(0, 500)) +
-  labs(x = "Week", y = "Locomotor activity (# crossing / 10 min)") +
-  theme_bw()  # Tema de fondo blanco
-s
-
-_____________________________________________________________
-# Agregar capas del gráfico q a p
-combined_plot <- p +
-  geom_point(data = fus_air, aes(x = variable, y = value), color = "blue") +
-  geom_line(data = fus_air, aes(x = variable, y = value, group = id), color = "blue") +
-  #coord_cartesian(ylim = c(0, 500))  # Ajustar límites del eje y
-  theme(legend.title = element_text())
-
-# Mostrar el gráfico combinado
-print(combined_plot)
-
-
-
+#Adding the sheat with air data to the first plot
 combined_plot <- p +
   geom_point(data = fus_air, aes(x = variable, y = value), color = "#76BD6A", alpha=0.6) +
   geom_line(data = fus_air, aes(x = variable, y = value, group = id), color = "#76BD6A", alpha=0.5) +
   #coord_cartesian(ylim = c(0, 500))  # Ajustar límites del eje y
-  theme(legend.title = element_text())
+  theme(legend.title = element_blank())
 
-# Mostrar el gráfico combinado
+#Printing the combined plot
 print(combined_plot)
 
+#Saving the combined plot
 ggsave("grafico.tiff", plot = combined_plot, device = "tiff", compression = "lzw")
 
-# Posterior modifications, such as addition of asterisks, hashtags and text modifica0tion was made by using the Inkscape software (https://inkscape.org)
+# Posterior modifications, such as addition of asterisks, hashtags and text modification were made by using the Inkscape software (https://inkscape.org)
 
 
 
